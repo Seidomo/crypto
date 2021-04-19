dd5eafab4571bf168a3a1e47c7ac30c12ff499cc
nickdeans <nickabramowicz@gmail.com>
2021-04-19 12:32:20 -0700

    created components

--- components/dashboard-view/Dashboard.js
+++ components/dashboard-view/Dashboard.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Dashboard from './components/dashboard-view/Dashboard.js'
 
-export default function App() {
+export default function Dashboard() {
   return (
     <View style={styles.container}>
-      <Dashboard />
+      <Text>Open up App.js to start working on your app!!!!!!</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
